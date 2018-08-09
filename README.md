a46945736fafc2e1e3fd6eee3f1f096ab5f5b7ba
glumac <mglumac@gmail.com>
2018-08-09 13:32:58 -0400

    cha cha cha cha changes

--- README.md
+++ README.md
@@ -1,31 +1,29 @@
-# jQuery: Duck-Hunt lab using jQuery!
+# JavaScript Duck-Hunt lab!
 
 ## Introduction
 
 ![](https://media.giphy.com/media/TWxWXeuF8rIhG/giphy.gif)
 
-Ok, projects are coming up so lets keep building games!
-
-For this lab, you'll be building the classic nintendo game, DUCK HUNT, using HTML, CSS, and jQuery.
+For this exercise, you'll be building the classic nintendo game, DUCK HUNT, using HTML, CSS, and JavaSCript.
 
 ## Exercise
 
 #### Starter Code
 
-- You have been provided with starter code, so you will ONLY need to work in the duckhunt.js file. 
-- To make this lab more managable, it is broken into 3 parts. Additionally, you have been given guidance on each step. 
+- You have been provided with starter code, so you will ONLY need to work in the duckhunt.js file and duckhunt.css (only making directed changes in css file).
+- To make this activity more managable, it is broken into 3 parts. Additionally, you have been given guidance on each step.
 
-#### Here are some __tips/hints__ to get started:
+#### Here are some **tips/hints** to get started:
 
-- GOOGLE! 
+- GOOGLE!
 - USE MDN JAVASCRIPT!
 - TALK TO YOUR CLASSMATES!
 
 ## Submission
 
-This assignment is due **Sunday, May 6th by 11:00 PM**
+This assignment is due **Sunday, August 12th by 11:00 PM**
 
-Please find some screenshots of what you'll be creating.  Feel free to get creative with how you style your interface.
+Please find some screenshots of what you'll be creating. Feel free to get creative with how you style your interface.
 
 ![Screen-shot](./images/duck-hunt-part-1.png) - Part 1
 ![Screen-shot](./images/duck-hunt-part-2.png) - Part 2
--- duckhunt.js
+++ duckhunt.js
@@ -1,19 +1,22 @@
 $(function() {
-  // I'll do some of the work for you, first I will grab the <body></body>
-  const $body = $('body');
+  const body = document.body;
 
-  // 1. Create a <div> with the class "duck" and add it to the body.
- 
+  // 1. Create a <div> with the class "duck" and add it to the body.  Do this step by step
+  // ( 1. create the element
+  //   2. add a class to the element
+  //   3. append the element to the body )
+
   // 2. Next, use setInterval to toggle the "flap" class on the duck every 250 ms (1/4 second)
- 
+  // https://www.w3schools.com/jsref/met_win_setinterval.asp
+
   // 3. Fantastic!  Now, let's move the duck using CSS "top" and "left". Create
   // a function `moveDuck` that takes a duck object as an argument and sets the
   // "top" and "left" CSS properties.
   // HINT: Use Math.random() * window.innerWidth    for "left"
   //       And Math.random() * window.innerHeight   for "top"
-  
-  // 4. Try making the duck move to a different location every second
- 
+
+  // 4. Try making the duck move to a different location every second (what did we use to do this several lines up??)
+
   // 5. Congratulations! Move on to part 2!
 
   // ---------------------------- PART 2 ---------------------------------
@@ -24,7 +27,6 @@ $(function() {
 
   // 7. Now, let's create lots of ducks!  Use a "for" loop to create 5 ducks
   //    using our fancy new createDuck() function
-
 
   // 8. Uh oh, our ducks are overlapping.  Modify createDuck so each time
   //     it creates a duck, it appears in a random location
@@ -39,16 +41,17 @@ $(function() {
   //     the duck when you click on it!
 
   // 12. After a duck has been clicked on, remove it from the DOM after
-  //     a short delay (1 second)
+  //     a short delay (1 second) Hint Hint...use setTimeout
+  //     as for removing the element check out https://dzone.com/articles/removing-element-plain
 
   // 13. Create a new function named checkForWinner() that reads the DOM
-  //     to see if there are any ducks left. If not, alert "YOU WIN!"
+  //     to see if there are any ducks left. (How can we check the DOM for more than one element?, and how can we see how many elements we get back) If not, alert "YOU WIN!"
 
   // 14. BONUS: The ducks are moving pretty erratically, can you think
   //     of a way to adjust the ducks speed based on how far needs to move?
 
   // 15. BONUS: Add the "left" and "right" class to the duck based on the
-  //     direction the duck is flying
+  //     direction the duck is flying and change the way the duck is facing
 
   // FIN. You win 1 trillion tokens.  Play the day away!
 });
